9f780f9c5eb2f3705e901f8e57a21151f7817e4f
Pradheep P <vidhyaprakash2510@gmail.com>
2025-03-23 11:55:01 +0530

    added count imeplementation

---
+++ backend/cpp_pradheep/count_implementation.cpp
@@ -0,0 +1,42 @@
+#include "json.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+#include <climits>
+
+using namespace std;
+
+struct Zone {
+    tuple<int, int, int> dimensions;
+    vector<string> containers_id;
+    int count;
+    bool temp_zone;
+    string temp_container_id;
+};
+
+unordered_map<string, Zone> zones;
+
+void main(){
+    cout<<"Implementing count implementation..."<<endl;
+
+    string json_data = R"(
+        [
+            {"Item ID": "001", "Preferred Zone": "Crew Quarters"},
+            {"Item ID": "002", "Preferred Zone": "Airlock"},
+            {"Item ID": "003", "Preferred Zone": "Medical Bay"},
+            {"Item ID": "004", "Preferred Zone": "Crew Quarters"}
+    ])";
+
+    json items = json::parse(json_data);
+
+    for (auto& item : items) {
+        string zoneId = item["Preferred Zone"];
+        zones[zoneId].count++;
+    }
+
+    for(auto& zone : zones){
+        cout<<zone.first<<" : "<<zone.second.count<<endl;
+    }
+}
